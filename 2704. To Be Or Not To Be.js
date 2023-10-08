8fefa290e1bd214071def597fbd8346bb0bf0f08
Yash Menaria <107399779+Yashmenaria1@users.noreply.github.com>
2023-10-08 17:10:44 +0530

    Create 2704. To Be Or Not To Be.js

---
+++ 2704. To Be Or Not To Be.js
@@ -0,0 +1,25 @@
+var expect = function(val) {
+    return {
+        toBe: (val2) => {
+            if (val !== val2) throw new Error("Not Equal");
+            else return true;
+        },
+        notToBe: (val2) => {
+            if (val === val2) throw new Error("Equal");
+            else return true;
+        }
+    }
+};
+//typescript
+function expect(val: any): ToBeOrNotToBe {
+	return {
+        toBe: (val2) => {
+            if (val !== val2) throw new Error("Not Equal");
+            else return true;
+        },
+        notToBe: (val2) => {
+            if (val === val2) throw new Error("Equal");
+            else return true;
+        }
+    }
+};
